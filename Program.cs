9ee7f8c66fae7086b371415060dbd037ca141551
jarabt <45106556+jarabt@users.noreply.github.com>
2019-08-06 17:46:56 +0200

    Zdenkuv puvodni soubor

--- Program.cs
+++ Program.cs
@@ -5,22 +5,26 @@ namespace Cgame
 {
     class Program
     {
-        private static Room currentRoom;
+        public static Room currentRoom;
         public static Player Player { get; set; }
-        
         static void Main(string[] args)
         {
-            var chest = new Chest("Stara truhla", 42);
-
-            Room[,] Rooms = new Room[10, 10];
-
-            Rooms[0,0] = new Room("Jste v temne mistnosti plne pavouku.", new List<Thing> { chest });
-
             Player = new Player(100, "Pepa");
             Player.inventory.Add(new Key("Stary klic", 42));
+            var chest = new Chest("Stara truhla", 42, new Sword("Stary mec", 10));
+            var dirtyChest = new Chest("Spinava truhla", 43, new Key("Novy klic", 101));
+            Player.inventory.Add(new Key("Spinavy klic", 43));
 
-            currentRoom = Rooms[0, 0];
-         
+            Room startRoom=    new Room("Jste v temne mistnosti plne pavouku.", new List<Thing> {chest});
+            Room anotherRoom = new Room("Jste v spinave mistnosti plne pavouku.", new List<Thing> { dirtyChest });
+            Room lockedRoom = new Room("Jste v nove mistnosti plne pavouku.", new List<Thing> { });
+            currentRoom = startRoom;
+
+        /*var door1 = */new Door("Spinave dvere", startRoom, anotherRoom, Door.State.OPEN, null);
+            var door2 = new Door("Funglovky nove bezpecnostni dvere", startRoom, lockedRoom, Door.State.BLOCKED, 101);
+            var case1 = new Blockage("Skrinka. Kdo dal skrinku pred dvere?", door2);
+            startRoom.Items.Add(case1);
+
             Console.WriteLine("Hra začíná");
 
             while (true)
@@ -51,6 +55,19 @@ namespace Cgame
                         var playerItem = Player.inventory[Convert.ToInt32(playerItemIndex)];
                         var roomItem = currentRoom.Items[Convert.ToInt32(roomItemIndex)];
                         roomItem.UseOnMe(playerItem);
+                        break;
+
+                    case "go":
+                        Console.WriteLine("Vyber skrz ktere dvere chces projit:");
+                        string roomItemIndex1 = Console.ReadLine();
+                        var roomItemGo = currentRoom.Items[Convert.ToInt32(roomItemIndex1)];
+                        if (roomItemGo is Door)
+                        {
+                            var d2 = roomItemGo as Door;
+                            if (d2 != null)
+                               if (d2.Go()) Console.WriteLine(currentRoom.ToString());
+                        }
+                        else Console.WriteLine($"Skrz {roomItemGo} nejde projit"); 
                         break;
 
                     default:
